db39207b651775bfa3492891b1fd1a5886fe249f
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-02-04 15:30:54 +0100

    Update NetherChestedClient.java

--- 1.20.4/Common/src/main/java/fuzs/netherchested/client/NetherChestedClient.java
+++ 1.20.4/Common/src/main/java/fuzs/netherchested/client/NetherChestedClient.java
@@ -7,8 +7,8 @@ import fuzs.netherchested.world.level.block.entity.NetherChestBlockEntity;
 import fuzs.puzzleslib.api.client.core.v1.ClientModConstructor;
 import fuzs.puzzleslib.api.client.core.v1.context.BlockEntityRenderersContext;
 import fuzs.puzzleslib.api.client.core.v1.context.BuiltinModelItemRendererContext;
+import fuzs.puzzleslib.api.client.core.v1.context.MenuScreensContext;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.blockentity.ChestRenderer;
 import net.minecraft.core.BlockPos;
@@ -18,20 +18,24 @@ import net.minecraft.world.item.ItemStack;
 public class NetherChestedClient implements ClientModConstructor {
 
     @Override
-    public void onClientSetup() {
-        MenuScreens.register(ModRegistry.NETHER_CHEST_MENU_TYPE.value(), NetherChestScreen::new);
-    }
-
-    @Override
     public void onRegisterBlockEntityRenderers(BlockEntityRenderersContext context) {
         context.registerBlockEntityRenderer(ModRegistry.NETHER_CHEST_BLOCK_ENTITY_TYPE.value(), ChestRenderer::new);
     }
 
     @Override
+    public void onRegisterMenuScreens(MenuScreensContext context) {
+        context.registerMenuScreen(ModRegistry.NETHER_CHEST_MENU_TYPE.value(), NetherChestScreen::new);
+    }
+
+    @Override
     public void onRegisterBuiltinModelItemRenderers(BuiltinModelItemRendererContext context) {
-        NetherChestBlockEntity netherChest = new NetherChestBlockEntity(BlockPos.ZERO, ModRegistry.NETHER_CHEST_BLOCK.value().defaultBlockState());
+        NetherChestBlockEntity netherChest = new NetherChestBlockEntity(BlockPos.ZERO,
+                ModRegistry.NETHER_CHEST_BLOCK.value().defaultBlockState()
+        );
         context.registerItemRenderer((ItemStack stack, ItemDisplayContext mode, PoseStack matrices, MultiBufferSource vertexConsumers, int light, int overlay) -> {
-            Minecraft.getInstance().getBlockEntityRenderDispatcher().renderItem(netherChest, matrices, vertexConsumers, light, overlay);
+            Minecraft.getInstance()
+                    .getBlockEntityRenderDispatcher()
+                    .renderItem(netherChest, matrices, vertexConsumers, light, overlay);
         }, ModRegistry.NETHER_CHEST_BLOCK.value());
     }
 }
