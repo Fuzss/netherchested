d51b7adf602640b765310bc2490637f73de90919
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-02-07 16:48:27 +0100

    prepare 1.21.4 port

---
+++ 1.21.4/Common/src/main/java/fuzs/netherchested/NetherChested.java
@@ -0,0 +1,36 @@
+package fuzs.netherchested;
+
+import fuzs.netherchested.config.ServerConfig;
+import fuzs.netherchested.init.ModRegistry;
+import fuzs.puzzleslib.api.config.v3.ConfigHolder;
+import fuzs.puzzleslib.api.core.v1.ModConstructor;
+import fuzs.puzzleslib.api.core.v1.context.BuildCreativeModeTabContentsContext;
+import fuzs.puzzleslib.api.core.v1.utility.ResourceLocationHelper;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTabs;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NetherChested implements ModConstructor {
+    public static final String MOD_ID = "netherchested";
+    public static final String MOD_NAME = "Nether Chested";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
+
+    public static final ConfigHolder CONFIG = ConfigHolder.builder(MOD_ID).server(ServerConfig.class);
+
+    @Override
+    public void onConstructMod() {
+        ModRegistry.bootstrap();
+    }
+
+    @Override
+    public void onBuildCreativeModeTabContents(BuildCreativeModeTabContentsContext context) {
+        context.registerBuildListener(CreativeModeTabs.FUNCTIONAL_BLOCKS, (itemDisplayParameters, output) -> {
+            output.accept(ModRegistry.NETHER_CHEST_ITEM.value());
+        });
+    }
+
+    public static ResourceLocation id(String path) {
+        return ResourceLocationHelper.fromNamespaceAndPath(MOD_ID, path);
+    }
+}
---
+++ 1.21.4/Fabric/src/main/java/fuzs/netherchested/fabric/NetherChestedFabric.java
@@ -0,0 +1,19 @@
+package fuzs.netherchested.fabric;
+
+import fuzs.limitlesscontainers.fabric.api.limitlesscontainers.v1.LimitlessSlotStorage;
+import fuzs.netherchested.NetherChested;
+import fuzs.netherchested.init.ModRegistry;
+import fuzs.netherchested.world.level.block.entity.NetherChestBlockEntity;
+import fuzs.puzzleslib.api.core.v1.ModConstructor;
+import net.fabricmc.api.ModInitializer;
+
+public class NetherChestedFabric implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        ModConstructor.construct(NetherChested.MOD_ID, NetherChested::new);
+        LimitlessSlotStorage.registerForBlockEntity(ModRegistry.NETHER_CHEST_BLOCK_ENTITY_TYPE.value(),
+                NetherChestBlockEntity::getContainer
+        );
+    }
+}
